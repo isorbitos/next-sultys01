670d54f7bc608f7d14ff87925420f8462a285574
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2021-04-26 18:46:18 +0300

    ready for first deploy

--- components/home-page/mastheader.js
+++ components/home-page/mastheader.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+import classes from './masterheader.module.css'
 
 const MasterHeader = () =>(
     <header className="masthead">
+        
+    
     <div className="container">
+   
         <div className="masthead-subheading text-uppercase">Čia gyvena skaniausios sultys</div>
         <div className="masthead-heading text-uppercase"></div>
         <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#portfolio">Sužinok daugiau</a>
